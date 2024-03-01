3a1ddcf17bdae702688e2ffd6257bb1e5e65885c
Francisco <fjkbravo@aol.com>
2024-03-01 14:29:08 -0500

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = []
+    cats.push("Milo", "Otis", "Garfield");
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    var name_1 = [...cats, name];
+    return name_1;
+}
+function prependCat(name){
+    var name_2 = [name, ...cats];
+    return name_2;
+}
+function removeLastCat(){
+    var newArray= cats.slice(0,cats.length-1);
+    return newArray;
+}
+function removeFirstCat(){
+    var newArray2= cats.slice(1);
+    return newArray2;
+}
